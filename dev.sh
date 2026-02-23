a834ac41cdcb37065244c4d6765deb90b11b6a9c
Rabiya Patan <rabiyapatan@Rabiyas-MacBook-Air.local>
2026-02-23 16:27:33 +0530

    fix dev.sh file

--- dev.sh
+++ dev.sh
@@ -1,13 +1,37 @@
 docker compose -f docker-compose.dev.yml up -d
 
+
+sleep 5
+
 cd backend
 cp .env.example .env
+sleep 1
 npm install
 
+sleep 1
+
 npx prisma migrate deploy
+
+sleep 1
 npx prisma generate
+
+
+sleep 2
 
 cd ../frontend
 cp .env.example .env
 npm install
+
+cd ..
+
+pwd
+
+(cd backend && 
+npm run start) &
+
+(cd frontend && 
+npm run dev )
+
+wait
+
 
